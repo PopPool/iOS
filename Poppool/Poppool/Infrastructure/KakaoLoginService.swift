8de2ea12f9df2c87f8bf95ac2e12a40ad29b203b
JunYoung <ghddns34@gmail.com>
2025-03-21 15:16:01 +0900

    [REFACTOR]: 메서드 분리, 주석 추가, error수정 및 일부 내용 수정의 커밋

--- Poppool/Poppool/Infrastructure/KakaoLoginService.swift
+++ Poppool/Poppool/Infrastructure/KakaoLoginService.swift
@@ -4,11 +4,6 @@ import KakaoSDKAuth
 
 final class KakaoLoginService: AuthServiceable {
     
-    struct Credential: Encodable {
-        var id: String
-        var token: String
-    }
-    
     var disposeBag = DisposeBag()
     
     func unlink() -> Observable<Void> {
@@ -48,22 +43,13 @@ final class KakaoLoginService: AuthServiceable {
                     line: #line
                 )
                 
-                // 카카오톡 미설치시 계정으로 접속 시도
-                UserApi.shared.loginWithKakaoAccount { [weak self] (oauthToken, error) in
-                    if let error = error {
-                        observer.onError(error)
-                    } else {
-                        if let self = self, let accessToken = oauthToken?.accessToken {
-                            self.fetchUserId(observer: observer, accessToken: accessToken)
-                        }
-                    }
-                }
-                
+                // 카카오톡 미설치시 웹으로 인증 시도
+                loginWithKakaoTalkWeb(observer: observer)
                 return Disposables.create()
             }
-            
-            // token을 획득하기 위한 로그인
-            loginWithKakaoTalk(observer: observer)
+    
+            // 카카오톡 설치시 앱으로 인증 시도
+            loginWithKakaoTalkApp(observer: observer)
             
             return Disposables.create()
         }
@@ -72,64 +58,46 @@ final class KakaoLoginService: AuthServiceable {
 
 private extension KakaoLoginService {
     
+    /// 제공된 액세스 토큰을 사용하여 사용자의 카카오 ID를 가져옵니다.
+    /// - Parameters:
+    ///   - observer: 인증 응답을 처리할 옵저버.
+    ///   - accessToken: 카카오 로그인 과정에서 얻은 액세스 토큰.
     func fetchUserId(observer: AnyObserver<AuthServiceResponse>, accessToken: String) {
         UserApi.shared.me() { user, error in
             if let error = error {
-                observer.onError(AuthError.unknownError)
+                observer.onError(AuthError.unknownError(description: error.localizedDescription))
             } else {
-                // ???: 여기 onComplete로 observer를 종료하지 않아도 되는지?
-                // ???: disposeBag으로 수거를 하지 않게 되는데 observer 수거에 대한 문제 발생 가능 여부 확인이 필요해보임
                 observer.onNext(.init(kakaoUserId: user?.id, kakaoAccessToken: accessToken))
-            }
-        }
-    }
-
-    func loginWithKakaoTalk(observer: AnyObserver<AuthServiceResponse>) {
-        UserApi.shared.loginWithKakaoTalk { oauthToken, error in
-            if let error = error {
-                Logger.log(
-                    message: "KakaoTalk Login Fail",
-                    category: .error,
-                    fileName: #file,
-                    line: #line
-                )
-                observer.onError(AuthError.unknownError)
-            } else {
-                if let oauthToken = oauthToken {
-                    Logger.log(
-                        message: "KakaoTalk Login Response - \(oauthToken)",
-                        category: .info,
-                        fileName: #file,
-                        line: #line
-                    )
-                    self.fetchUserId(observer: observer, accessToken: oauthToken.accessToken)
-                }
-                
-
+                observer.onCompleted()
             }
         }
     }
     
-    func fetchUserProfile() -> Single<User> {
-        // MARK: 이런 식으로 구현하면 될거 같다 생각만 하고 여기서 멈췄습니다... ㅠㅠ
-//        return Observable
-//            .just(<#T##element: _##_#>)
-//            .asSingle()
-        return UserApi.shared.rx.me()
-            .do { user in
-                Logger.log(
-                    message: "KakaoTalk Profile Response - \(user)",
-                    category: .info,
-                    fileName: #file,
-                    line: #line
-                )
-            } onError: { _ in
-                Logger.log(
-                    message: "KakaoTalk Profile Fetch Fail",
-                    category: .error,
-                    fileName: #file,
-                    line: #line
-                )
+    /// 카카오톡 앱을 사용하여 로그인하고 액세스 토큰을 가져옵니다.
+    /// - Parameter observer: 인증 응답을 처리할 옵저버.
+    func loginWithKakaoTalkApp(observer: AnyObserver<AuthServiceResponse>) {
+        UserApi.shared.loginWithKakaoTalk { [weak self] oauthToken, error in
+            if let error = error {
+                observer.onError(AuthError.unknownError(description: error.localizedDescription))
+            } else {
+                if let accessToken = oauthToken?.accessToken {
+                    self?.fetchUserId(observer: observer, accessToken: accessToken)
+                }
             }
+        }
+    }
+    
+    /// 카카오톡 웹을 사용하여 로그인하고 액세스 토큰을 가져옵니다.
+    /// - Parameter observer: 인증 응답을 처리할 옵저버.
+    func loginWithKakaoTalkWeb(observer: AnyObserver<AuthServiceResponse>) {
+        UserApi.shared.loginWithKakaoAccount { [weak self] (oauthToken, error) in
+            if let error = error {
+                observer.onError(AuthError.unknownError(description: error.localizedDescription))
+            } else {
+                if let accessToken = oauthToken?.accessToken {
+                    self?.fetchUserId(observer: observer, accessToken: accessToken)
+                }
+            }
+        }
     }
 }
