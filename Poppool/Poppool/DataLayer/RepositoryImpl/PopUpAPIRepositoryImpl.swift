fa9db31f7b2c124a010d0029c20f5bef796ccdc1
JunYoung <ghddns34@gmail.com>
2025-04-17 22:23:49 +0900

    refactor/#116: PopUpAPI 관련 Domain Layer DTO 제거 - 빌드 확인

--- Poppool/Poppool/DataLayer/RepositoryImpl/PopUpAPIRepositoryImpl.swift
+++ Poppool/Poppool/DataLayer/RepositoryImpl/PopUpAPIRepositoryImpl.swift
@@ -11,33 +11,60 @@ final class PopUpAPIRepositoryImpl: PopUpAPIRepository {
         self.provider = provider
     }
 
-    func postBookmarkPopUp(request: PostBookmarkPopUpRequestDTO) -> Completable {
+    func postBookmarkPopUp(popUpStoreId: Int64) -> Completable {
+        let request = PostBookmarkPopUpRequestDTO(popUpStoreId: popUpStoreId)
         let endPoint = UserAPIEndPoint.postBookmarkPopUp(request: request)
         return provider.request(with: endPoint, interceptor: tokenInterceptor)
     }
 
-    func getClosePopUpList(request: GetSearchPopUpListRequestDTO) -> Observable<GetClosePopUpListResponseDTO> {
+    func getClosePopUpList(
+        categories: String?,
+        page: Int32?,
+        size: Int32?,
+        sort: String?,
+        query: String?,
+        sortCode: String?
+    ) -> Observable<GetSearchBottomPopUpListResponse> {
+        let request = GetSearchPopUpListRequestDTO(categories: categories, page: page, size: size, sort: sort, query: query, sortCode: sortCode)
         let endPoint = PopUpAPIEndPoint.getClosePopUpList(request: request)
-        return provider.requestData(with: endPoint, interceptor: tokenInterceptor)
+        return provider.requestData(with: endPoint, interceptor: tokenInterceptor).map { $0.toDomain() }
     }
 
-    func getOpenPopUpList(request: GetSearchPopUpListRequestDTO) -> Observable<GetOpenPopUpListResponseDTO> {
+    func getOpenPopUpList(
+        categories: String?,
+        page: Int32?,
+        size: Int32?,
+        sort: String?,
+        query: String?,
+        sortCode: String?
+    ) -> Observable<GetSearchBottomPopUpListResponse> {
+        let request = GetSearchPopUpListRequestDTO(categories: categories, page: page, size: size, sort: sort, query: query, sortCode: sortCode)
         let endPoint = PopUpAPIEndPoint.getOpenPopUpList(request: request)
-        return provider.requestData(with: endPoint, interceptor: tokenInterceptor)
+        return provider.requestData(with: endPoint, interceptor: tokenInterceptor).map { $0.toDomain() }
     }
 
-    func getSearchPopUpList(request: GetSearchPopUpListRequestDTO) -> Observable<GetSearchPopUpListResponseDTO> {
+    func getSearchPopUpList(
+        categories: String?,
+        page: Int32?,
+        size: Int32?,
+        sort: String?,
+        query: String?,
+        sortCode: String?
+    ) -> Observable<GetSearchPopUpListResponse> {
+        let request = GetSearchPopUpListRequestDTO(categories: categories, page: page, size: size, sort: sort, query: query, sortCode: sortCode)
         let endPoint = PopUpAPIEndPoint.getSearchPopUpList(request: request)
-        return provider.requestData(with: endPoint, interceptor: tokenInterceptor)
+        return provider.requestData(with: endPoint, interceptor: tokenInterceptor).map { $0.toDomain() }
     }
 
-    func getPopUpDetail(request: GetPopUpDetailRequestDTO) -> Observable<GetPopUpDetailResponseDTO> {
+    func getPopUpDetail(commentType: String?, popUpStoreId: Int64, viewCountYn: Bool?) -> Observable<GetPopUpDetailResponse> {
+        let request = GetPopUpDetailRequestDTO(commentType: commentType, popUpStoreId: popUpStoreId, viewCountYn: viewCountYn)
         let endPoint = PopUpAPIEndPoint.getPopUpDetail(request: request)
-        return provider.requestData(with: endPoint, interceptor: tokenInterceptor)
+        return provider.requestData(with: endPoint, interceptor: tokenInterceptor).map { $0.toDomain() }
     }
 
-    func getPopUpComment(request: GetPopUpCommentRequestDTO) -> Observable<GetPopUpCommentResponseDTO> {
+    func getPopUpComment(commentType: String?, page: Int32?, size: Int32?, sort: String?, popUpStoreId: Int64) -> Observable<GetPopUpCommentResponse> {
+        let request = GetPopUpCommentRequestDTO(commentType: commentType, page: page, size: size, sort: sort, popUpStoreId: popUpStoreId)
         let endPoint = PopUpAPIEndPoint.getPopUpComment(request: request)
-        return provider.requestData(with: endPoint, interceptor: tokenInterceptor)
+        return provider.requestData(with: endPoint, interceptor: tokenInterceptor).map { $0.toDomain() }
     }
 }
--- Poppool/Poppool/DomainLayer/DomainInterface/Repository/PopUpAPIRepository.swift
+++ Poppool/Poppool/DomainLayer/DomainInterface/Repository/PopUpAPIRepository.swift
@@ -3,10 +3,42 @@ import Foundation
 import RxSwift
 
 protocol PopUpAPIRepository {
-    func postBookmarkPopUp(request: PostBookmarkPopUpRequestDTO) -> Completable
-    func getClosePopUpList(request: GetSearchPopUpListRequestDTO) -> Observable<GetClosePopUpListResponseDTO>
-    func getOpenPopUpList(request: GetSearchPopUpListRequestDTO) -> Observable<GetOpenPopUpListResponseDTO>
-    func getSearchPopUpList(request: GetSearchPopUpListRequestDTO) -> Observable<GetSearchPopUpListResponseDTO>
-    func getPopUpDetail(request: GetPopUpDetailRequestDTO) -> Observable<GetPopUpDetailResponseDTO>
-    func getPopUpComment(request: GetPopUpCommentRequestDTO) -> Observable<GetPopUpCommentResponseDTO>
+    func postBookmarkPopUp(popUpStoreId: Int64) -> Completable
+
+    func getClosePopUpList(
+        categories: String?,
+        page: Int32?,
+        size: Int32?,
+        sort: String?,
+        query: String?,
+        sortCode: String?
+    ) -> Observable<GetSearchBottomPopUpListResponse>
+
+    func getOpenPopUpList(
+        categories: String?,
+        page: Int32?,
+        size: Int32?,
+        sort: String?,
+        query: String?,
+        sortCode: String?
+    ) -> Observable<GetSearchBottomPopUpListResponse>
+
+    func getSearchPopUpList(
+        categories: String?,
+        page: Int32?,
+        size: Int32?,
+        sort: String?,
+        query: String?,
+        sortCode: String?
+    ) -> Observable<GetSearchPopUpListResponse>
+
+    func getPopUpDetail(commentType: String?, popUpStoreId: Int64, viewCountYn: Bool?) -> Observable<GetPopUpDetailResponse>
+
+    func getPopUpComment(
+        commentType: String?,
+        page: Int32?,
+        size: Int32?,
+        sort: String?,
+        popUpStoreId: Int64
+    ) -> Observable<GetPopUpCommentResponse>
 }
