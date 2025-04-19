1f4ce4867a9cbd328ae99002c634a4ddaa59975a
김기현 <creeps1104@gmail.com>
2025-04-20 08:29:35 +0900

    refactor/#117: Comment API 레포지토리 인터페이스에서 DTO 파라미터 제거 및 임플에서 매핑 빌드테스트 확인

--- Poppool/Poppool/DomainLayer/Domain/UseCaseImpl/CommentAPIUseCaseImpl.swift
+++ Poppool/Poppool/DomainLayer/Domain/UseCaseImpl/CommentAPIUseCaseImpl.swift
@@ -3,7 +3,7 @@ import Foundation
 import RxSwift
 
 final class CommentAPIUseCaseImpl: CommentAPIUseCase {
-    
+
     private let repository: CommentAPIRepository
 
     init(repository: CommentAPIRepository) {
@@ -11,14 +11,14 @@ final class CommentAPIUseCaseImpl: CommentAPIUseCase {
     }
 
     func postCommentAdd(popUpStoreId: Int64, content: String?, commentType: String?, imageUrlList: [String?]) -> Completable {
-        return repository.postCommentAdd(request: .init(popUpStoreId: popUpStoreId, content: content, commentType: commentType, imageUrlList: imageUrlList))
+        return repository.postCommentAdd(popUpStoreId: popUpStoreId, content: content, commentType: commentType, imageUrlList: imageUrlList)
     }
 
     func deleteComment(popUpStoreId: Int64, commentId: Int64) -> Completable {
-        return repository.deleteComment(request: .init(popUpStoreId: popUpStoreId, commentId: commentId))
+        return repository.deleteComment(popUpStoreId: popUpStoreId, commentId: commentId)
     }
 
     func editComment(popUpStoreId: Int64, commentId: Int64, content: String?, imageUrlList: [PutCommentImageDataRequestDTO]?) -> Completable {
-        return repository.editComment(request: .init(popUpStoreId: popUpStoreId, commentId: commentId, content: content, imageUrlList: imageUrlList))
+        return repository.editComment(popUpStoreId: popUpStoreId, commentId: commentId, content: content, imageUrlList: imageUrlList)
     }
 }
--- Poppool/Poppool/DomainLayer/DomainInterface/UseCase/CommentAPIUseCase.swift
+++ Poppool/Poppool/DomainLayer/DomainInterface/UseCase/CommentAPIUseCase.swift
@@ -5,5 +5,5 @@ import RxSwift
 protocol CommentAPIUseCase {
     func postCommentAdd(popUpStoreId: Int64, content: String?, commentType: String?, imageUrlList: [String?]) -> Completable
     func deleteComment(popUpStoreId: Int64, commentId: Int64) -> Completable
-    func editComment(popUpStoreId: Int64, commentId: Int64, content: String?, imageUrlList: [PutCommentImageDataRequestDTO]?) -> Completable
+    func editComment(popUpStoreId: Int64, commentId: Int64, content: String?, imageUrlList: [String?]?) -> Completable
 }
