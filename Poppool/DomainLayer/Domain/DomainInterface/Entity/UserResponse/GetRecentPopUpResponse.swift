01e2cff4ecd4c51904a48c5445b71200fca94354
0Hooni <thddudgns972@gmail.com>
2025-04-21 02:05:36 +0900

    remove/#112: 사용하지 않는 Domain 컨버터 제거

--- Poppool/DomainLayer/Domain/DomainInterface/Entity/UserResponse/GetRecentPopUpResponse.swift
+++ Poppool/DomainLayer/Domain/DomainInterface/Entity/UserResponse/GetRecentPopUpResponse.swift
@@ -16,17 +16,3 @@ public struct GetRecentPopUpDataResponse {
     var address: String?
     var closeYn: Bool
 }
-
-public extension GetRecentPopUpDataResponse {
-    func toStoreItem() -> StoreItem {
-        return StoreItem(
-            id: self.popUpStoreId,
-            thumbnailURL: self.mainImageUrl ?? "",
-            category: "카테고리",
-            title: self.popUpStoreName ?? "제목 없음",
-            location: self.address ?? "주소 없음",
-            dateRange: "\(self.startDate ?? "") ~ \(self.endDate ?? "")",
-            isBookmarked: self.closeYn
-        )
-    }
-}
