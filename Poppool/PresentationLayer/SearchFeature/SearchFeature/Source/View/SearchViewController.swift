55e2f4181984e625bd53a40f7883bb60784da75a
0Hooni <thddudgns972@gmail.com>
2025-05-02 11:12:20 +0900

    feat/#131: SearchFeature 모듈 추가
    - 별도의 DemoApp까지 추가

---
+++ Poppool/PresentationLayer/SearchFeature/SearchFeature/Source/View/SearchViewController.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+public class SearchViewController: UIViewController {
+
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
