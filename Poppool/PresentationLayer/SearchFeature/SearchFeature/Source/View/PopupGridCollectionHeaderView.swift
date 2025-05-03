c185aef2d139c7f2ece4f1bc8c08e4468921f6f9
0Hooni <thddudgns972@gmail.com>
2025-05-03 22:26:15 +0900

    feat/#131: 팝업 그리드 헤더 뷰 추가

---
+++ Poppool/PresentationLayer/SearchFeature/SearchFeature/Source/View/PopupGridCollectionHeaderView.swift
@@ -0,0 +1,92 @@
+import UIKit
+
+import DesignSystem
+
+import SnapKit
+import RxSwift
+import Then
+
+final class PopupGridCollectionHeaderView: UICollectionReusableView {
+    // MARK: - Properties
+
+    var disposeBag = DisposeBag()
+
+    private let cellCountLabel = PPLabel(style: .regular, fontSize: 13).then {
+        $0.textColor = .g400
+    }
+
+    private let sortedTitleLabel = PPLabel(style: .regular, fontSize: 13)
+
+    private let dropDownImageView = UIImageView().then {
+        $0.image = UIImage(named: "icon_dropdown")
+        $0.isUserInteractionEnabled = false
+
+    }
+
+    let sortedButton = UIButton()
+
+    // MARK: - init
+    init() {
+        super.init(frame: .zero)
+
+        self.addViews()
+        self.setupConstraints()
+        self.configureUI()
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("\(#file), \(#function) Error")
+    }
+}
+
+// MARK: - SetUp
+private extension PopupGridCollectionHeaderView {
+    func addViews() {
+        [cellCountLabel, sortedButton].forEach {
+            addSubview($0)
+        }
+
+        [sortedTitleLabel, dropDownImageView].forEach {
+            sortedButton.addSubview($0)
+        }
+    }
+
+    func setupConstraints() {
+        cellCountLabel.snp.makeConstraints { make in
+            make.leading.equalToSuperview()
+            make.verticalEdges.equalToSuperview()
+        }
+
+        sortedButton.snp.makeConstraints { make in
+            make.trailing.equalToSuperview()
+            make.verticalEdges.equalToSuperview()
+        }
+
+        sortedTitleLabel.snp.makeConstraints { make in
+            make.leading.equalToSuperview()
+            make.verticalEdges.equalToSuperview()
+        }
+
+        dropDownImageView.snp.makeConstraints { make in
+            make.verticalEdges.equalToSuperview()
+            make.width.equalTo(dropDownImageView.snp.height)
+            make.leading.equalTo(sortedTitleLabel.snp.trailing).offset(6)
+            make.trailing.equalToSuperview()
+        }
+    }
+
+    func configureUI() { }
+}
+
+extension PopupGridCollectionHeaderView: Inputable {
+    struct Input {
+        var count: Int64
+        var sortedTitle: String?
+    }
+
+    func injection(with input: Input) {
+        sortedTitleLabel.text = input.sortedTitle
+        cellCountLabel.text = "총 \(input.count)개"
+    }
+}
