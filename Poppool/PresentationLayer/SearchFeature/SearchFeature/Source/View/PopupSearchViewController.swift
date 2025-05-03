f0c651f230ffaa07293a7568ca1d4fb0d639a897
0Hooni <thddudgns972@gmail.com>
2025-05-03 15:08:39 +0900

    fix/#131: 팝업 검색 ViewController 수정
    - 다른 ViewController의 컨벤션과 동일하게 하나의 mainView를 사용하도록 함

--- Poppool/PresentationLayer/SearchFeature/SearchFeature/Source/View/PopupSearchViewController.swift
+++ Poppool/PresentationLayer/SearchFeature/SearchFeature/Source/View/PopupSearchViewController.swift
@@ -12,48 +12,23 @@ public final class PopupSearchViewController: BaseViewController, View {
     // MARK: - Properties
     public var disposeBag = DisposeBag()
 
-    private let searchBar = PPSearchBarView()
+    private let mainView = PopupSearchView()
 }
 
 // MARK: - Life Cycle
 extension PopupSearchViewController {
+    public override func loadView() {
+        self.view = mainView
+    }
 
     public override func viewDidLoad() {
         super.viewDidLoad()
-
-        self.addViews()
-        self.setupConstraints()
-        self.configureUI()
     }
-}
-
-// MARK: - SetUp
-private extension PopupSearchViewController {
-    func addViews() {
-        self.view.addSubview(searchBar)
-    }
-
-    func setupConstraints() {
-        searchBar.snp.makeConstraints { make in
-            make.leading.equalToSuperview().inset(20)
-            make.trailing.equalToSuperview().inset(16)
-            make.top.equalTo(self.view.safeAreaLayoutGuide.snp.top).offset(12)
-        }
-    }
-
-    func configureUI() { }
 }
 
 // MARK: - Bind
 extension PopupSearchViewController {
     public func bind(reactor: Reactor) {
-        searchBar.cancelButton.rx.tap
-            .withUnretained(self)
-            .subscribe { (owner, _) in
-                owner.navigationController?.popViewController(animated: true)
-            }
-            .disposed(by: disposeBag)
+        
     }
 }
-
-extension PopupSearchViewController: UISearchBarDelegate { }
