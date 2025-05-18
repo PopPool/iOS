47f5066b5936ed2a0554a4209d598deed1f70d4d
0Hooni <thddudgns972@gmail.com>
2025-05-18 22:46:51 +0900

    refactor/#150: LayoutFactory에 빌더 패턴을 사용하도록 수정

--- Poppool/PresentationLayer/DesignSystem/DesignSystem/Components/Layout/GridCollectionLayoutProvider.swift
+++ Poppool/PresentationLayer/DesignSystem/DesignSystem/Components/Layout/GridCollectionLayoutProvider.swift
@@ -4,29 +4,14 @@ public struct GridCollectionLayoutProvider: CollectionLayoutProvidable {
     public init() { }
     
     public func makeLayout() -> NSCollectionLayoutSection {
-        // Item
-        let itemSize = NSCollectionLayoutSize(
-            widthDimension: .fractionalWidth(0.5),
-            heightDimension: .absolute(249)
-        )
-        let item = NSCollectionLayoutItem(layoutSize: itemSize)
-
-        // Group
-        let groupSize = NSCollectionLayoutSize(
-            widthDimension: .fractionalWidth(1.0),
-            heightDimension: .absolute(249)
-        )
-        let group = NSCollectionLayoutGroup.horizontal(
-            layoutSize: groupSize,
-            subitems: [item, item]
-        )
-        group.interItemSpacing = .fixed(16)
-
-        // Section
-        let section = NSCollectionLayoutSection(group: group)
-        section.contentInsets = NSDirectionalEdgeInsets(top: 16, leading: 20, bottom: 0, trailing: 20)
-        section.interGroupSpacing = 24
-
-        return section
+        return CollectionLayoutBuilder()
+            .item(width: .fractionalWidth(0.5), height: .absolute(249))
+            .group(width: .fractionalWidth(1.0), height: .absolute(249))
+            .numberOfItemsPerGroup(2)
+            .itemSpacing(16)
+            .composeSection(.horizontal)
+            .withContentInsets(top: 16, leading: 20, bottom: 0, trailing: 20)
+            .groupSpacing(24)
+            .build()
     }
-} 
+}
--- Poppool/PresentationLayer/DesignSystem/DesignSystem/Components/Layout/TagCollectionLayoutProvider.swift
+++ Poppool/PresentationLayer/DesignSystem/DesignSystem/Components/Layout/TagCollectionLayoutProvider.swift
@@ -4,40 +4,18 @@ public struct TagCollectionLayoutProvider: CollectionLayoutProvidable, HeaderLay
     public init() { }
     
     public func makeLayout() -> NSCollectionLayoutSection {
-        // Item
-        let itemSize = NSCollectionLayoutSize(
-            widthDimension: .estimated(100),
-            heightDimension: .absolute(31)
-        )
-        let item = NSCollectionLayoutItem(layoutSize: itemSize)
-
-        // Group
-        let groupSize = NSCollectionLayoutSize(
-            widthDimension: .estimated(100),
-            heightDimension: .estimated(31)
-        )
-        let group = NSCollectionLayoutGroup.horizontal(
-            layoutSize: groupSize,
-            subitems: [item]
-        )
-
-        // Section
-        let section = NSCollectionLayoutSection(group: group)
-        section.orthogonalScrollingBehavior = .continuous
-        section.interGroupSpacing = 6
-        
-        return section
+        return CollectionLayoutBuilder()
+            .item(width: .estimated(100), height: .absolute(31))
+            .group(width: .estimated(100), height: .estimated(31))
+            .composeSection(.vertical)
+            .withScrollingBehavior(.continuous)
+            .groupSpacing(6)
+            .build()
     }
     
     public func makeHeaderLayout(_ elementKind: String) -> NSCollectionLayoutBoundarySupplementaryItem {
-        let headerSize = NSCollectionLayoutSize(
-            widthDimension: .fractionalWidth(1.0),
-            heightDimension: .absolute(24)
-        )
-        return NSCollectionLayoutBoundarySupplementaryItem(
-            layoutSize: headerSize,
-            elementKind: elementKind,
-            alignment: .top
-        )
+        return CollectionLayoutBuilder()
+            .header(elementKind: elementKind, height: .absolute(24))
+            .buildHeader()
     }
 } 
