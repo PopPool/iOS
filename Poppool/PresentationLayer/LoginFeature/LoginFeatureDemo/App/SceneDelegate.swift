66b086de91d78fb5612a8f1926663404027c59cf
0Hooni <thddudgns972@gmail.com>
2025-07-21 17:25:12 +0900

    fix/#160: 로그인 데모 수정

--- Poppool/PresentationLayer/LoginFeature/LoginFeatureDemo/App/SceneDelegate.swift
+++ Poppool/PresentationLayer/LoginFeature/LoginFeatureDemo/App/SceneDelegate.swift
@@ -16,7 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         @Dependency var factory: LoginFactory
 
         navigationController.pushViewController(
-            factory.make(),
+            factory.make(.main, text: "간편하게 SNS 로그인하고\n팝풀 서비스를 이용해보세요"),
             animated: false
         )
 
