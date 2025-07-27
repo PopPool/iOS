3fa65c7a6d839bddc10bdd0176484f4b28d824d5
0Hooni <thddudgns972@gmail.com>
2025-07-27 20:49:36 +0900

    feat/#161: 아토믹 폰트 시스템 타입 정의

---
+++ Poppool/PresentationLayer/DesignSystem/DesignSystem/Font/FontSystem.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+public enum FontStyle {
+    // 한국어 Typeface Guide
+    case KOl32, KOl28, KOl24, KOl20, KOl18, KOl16, KOl15, KOl14, KOl13, KOl12, KOl11
+    case KOr32, KOr28, KOr24, KOr20, KOr18, KOr16, KOr15, KOr14, KOr13, KOr12, KOr11
+    case KOm32, KOm28, KOm24, KOm20, KOm18, KOm16, KOm15, KOm14, KOm13, KOm12, KOm11
+    case KOb32, KOb28, KOb24, KOb20, KOb18, KOb16, KOb15, KOb14, KOb13, KOb12, KOb11
+
+    // 영어 Typeface Guide
+    case ENl32, ENl28, ENl24, ENl20, ENl18, ENl16, ENl15, ENl14, ENl13, ENl12, ENl11
+    case ENr32, ENr28, ENr24, ENr20, ENr18, ENr16, ENr15, ENr14, ENr13, ENr12, ENr11
+    case ENm32, ENm28, ENm24, ENm20, ENm18, ENm16, ENm15, ENm14, ENm13, ENm12, ENm11
+    case ENb32, ENb28, ENb24, ENb20, ENb18, ENb16, ENb15, ENb14, ENb13, ENb12, ENb11
+}
