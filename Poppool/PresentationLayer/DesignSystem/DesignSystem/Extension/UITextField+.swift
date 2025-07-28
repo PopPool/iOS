8b32702942f77a505f2fdf3746cc7f5452161c8d
0Hooni <thddudgns972@gmail.com>
2025-07-28 17:28:37 +0900

    docs/#161: deprecated 메세지 추가

--- Poppool/PresentationLayer/DesignSystem/DesignSystem/Extension/UITextField+.swift
+++ Poppool/PresentationLayer/DesignSystem/DesignSystem/Extension/UITextField+.swift
@@ -1,6 +1,7 @@
 import UIKit
 
 public extension UITextField {
+    @available(*, deprecated, message: "직접 속성을 넣는 방법 대신 타이포시스템을 이용하는 `setPlaceholder(text:color:style:)`을 이용해주세요")
     func setPlaceholder(text: String, color: UIColor, font: UIFont) {
         self.attributedPlaceholder = NSAttributedString(
             string: text,
